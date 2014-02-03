e9bd7dc58317a1ee7eba42ac886ced2c250e5873
olivier.locci@gmail.com <olivier.locci@gmail.com@7e36b701-f3a1-d628-dd7f-39ffab74408c>
2014-02-03 16:13:01 +0000

    added jquery

--- trunk/index.php
+++ trunk/index.php
@@ -3,6 +3,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link href="style.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="js/sitejs.js"></script>
         <title></title>
     </head>
     <body>
--- trunk/site/header.php
+++ trunk/site/header.php
@@ -1,3 +1,13 @@
 <?php
-    echo 'header';
+    echo 'header<br>';
+    $i=0;
+    echo "<table>\n";
+    for($i=0; $i<10; $i++){
+        echo "\t<tr>\n";
+        for($j=0; $j<10; $j++){
+            echo "\t\t<td class=\"actiontd\" id=\"".$i."_".$j."\">".$i."_".$j."<td>\n";
+        }
+        echo "\t</tr>\n";
+    }
+    echo '</table>';
 ?>
